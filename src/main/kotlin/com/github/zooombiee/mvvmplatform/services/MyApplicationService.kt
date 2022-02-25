0261303090fea9099b15bad60f792ee6368754b1
GitHub Action <action@github.com>
2022-02-25 10:04:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/zooombiee/mvvmplatform/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/zooombiee/mvvmplatform/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.zooombiee.mvvmplatform.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zooombiee.mvvmplatform.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/zooombiee/mvvmplatform/services/MyProjectService.kt
+++ src/main/kotlin/com/github/zooombiee/mvvmplatform/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.zooombiee.mvvmplatform.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zooombiee.mvvmplatform.MyBundle
 
 class MyProjectService(project: Project) {
 
